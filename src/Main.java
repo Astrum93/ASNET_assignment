cebf2b8cd52451a784c3275bf36509d53643fafa
Astrum93 <jungmin2670@gmail.com>
2023-12-02 02:09:52 +0900

    asnet assignment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.*;
+import java.lang.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        WordFilter wordFilter = new WordFilter();
+        String userContents = wordFilter.usercontent;
+        String[] blackWordList = wordFilter.blackWordList;
+        System.out.println(wordFilter.result.toString());
     }
 }
--- src/WordFilter.java
+++ src/WordFilter.java
@@ -1,4 +1,5 @@
-import java.util.Map;
+import java.util.*;
+import java.lang.*;
 
 public class WordFilter {
     /**
@@ -9,8 +10,24 @@ public class WordFilter {
      * @return 금지어의 등장 횟수와 목록
      */
 
+    String usercontent = "오늘은 너무 힘든 하루 였다. 나무 숲, 바다, 파도, 나무나무나무 나 무";
+    String[] blackWordList = {"나무","숲"};
+    Map<String, Integer> result;
 
-    public Map<String, Integer> countBlackWords(String usercontent, String blackWordList) {
-        return null;
+    public Map<String, Integer> countBlackWords(String usercontent, String[] blackWordList) {
+        for (int i = 0;  i < blackWordList.length; i++){
+            System.out.println(blackWordList[i]);
+            if (usercontent.contains(blackWordList[i])){
+                System.out.println("있습니다.");
+                result.put("blackWordList[i]", 0);
+                System.out.println(result);
+            }
+            else {
+                System.out.println("없습니다.");
+            }
+        }
+        return result;
     }
 }
+
+
