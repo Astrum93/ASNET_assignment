39564f256833d68d72b2d67589f86dd0ea6d603b
Astrum93 <jungmin2670@gmail.com>
2023-12-01 21:34:51 +0900

    asnet assignment

---
+++ src/WordFilter.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+
+public class WordFilter {
+    /**
+     * 금지어 확인 함수.
+     *
+     * @param userContent 사용자의 글
+     * @param blackWordList 금지어 목록
+     * @return 금지어의 등장 횟수와 목록
+     */
+
+
+    public Map<String, Integer> countBlackWords(String usercontent, String blackWordList) {
+        return null;
+    }
+}
