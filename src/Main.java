70e1df64857a1537cd24459c49b2f1f251c8a3d7
Astrum93 <jungmin2670@gmail.com>
2023-12-03 16:49:06 +0900

    asnet assignment - 첫 번째 WordFilter 완료

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ import java.lang.*;
 public class Main {
     public static void main(String[] args) {
         WordFilter wordFilter = new WordFilter();
-        String userContents = wordFilter.usercontent;
+        String userContents = wordFilter.userContent;
         String[] blackWordList = wordFilter.blackWordList;
-        System.out.println(wordFilter.result.toString());
+
+        Map<String, Integer> report = wordFilter.countBlackWords(userContents, blackWordList);
+
+        for (Map.Entry<String, Integer> entry : report.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + "개 발견");
+        }
     }
 }
--- src/WordFilter.java
+++ src/WordFilter.java
@@ -10,21 +10,29 @@ public class WordFilter {
      * @return 금지어의 등장 횟수와 목록
      */
 
-    String usercontent = "오늘은 너무 힘든 하루 였다. 나무 숲, 바다, 파도, 나무나무나무 나 무";
+    String userContent = "오늘은 너무 힘든 하루 였다. 나무 숲, 바다, 파도, 나무 나무 나무 나 무 숲 숲 숲 숲";
     String[] blackWordList = {"나무","숲"};
-    Map<String, Integer> result;
+    Map<String, Integer> result = new HashMap<>();
 
-    public Map<String, Integer> countBlackWords(String usercontent, String[] blackWordList) {
-        for (int i = 0;  i < blackWordList.length; i++){
-            System.out.println(blackWordList[i]);
-            if (usercontent.contains(blackWordList[i])){
-                System.out.println("있습니다.");
-                result.put("blackWordList[i]", 0);
-                System.out.println(result);
+
+    public Map<String, Integer> countBlackWords(String userContent, String[] blackWordList) {
+        String[] userContentSplit = userContent.split(" ");
+        for (String blackword : blackWordList){
+            // System.out.println(blackword);
+            int wordCount = 0;
+            if (userContent.contains(blackword)) {
+                for (String splitContent : userContentSplit) {
+                    if (blackword.equals(splitContent)) {
+                        //System.out.println(blackword + " 발견.");
+                        wordCount++;
+                        result.put(blackword, wordCount);
+                    }
+                    // else System.out.println("발견 되지 않았습니다.");
+                }
             }
-            else {
-                System.out.println("없습니다.");
-            }
+           // else {
+               // System.out.println("없습니다.");
+           // }
         }
         return result;
     }
