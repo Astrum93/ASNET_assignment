953aa02c92261b0c04457faad6fcf06d91eb108b
Astrum93 <jungmin2670@gmail.com>
2023-12-03 17:44:03 +0900

    asnet assignment - 두 번째 WordFilter 완료

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,24 @@ public class Main {
         UserContents userContents = new UserContents();
         BlackWordList blackWordList = new BlackWordList();
 
-
-
+        // 첫 번째 WordFilter
         Map<String, Integer> report = wordFilter.countBlackWords(userContents.Contents, blackWordList.wordList);
+        // 두 번째 WordFilter(정규식)
         Map<String, Integer> report2 = wordFilter2.countBlackWords2(userContents.Contents2, blackWordList.wordList);
 
+        // 첫 번째 WordFilter 실행
+        System.out.println("첫 번째 WordFilter 시작");
         for (Map.Entry<String, Integer> entry : report.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue() + "개 발견");
         }
+        System.out.println("첫 번째 WordFilter 종료");
+        System.out.println(" ");
 
+        // 두 번째 WordFilter(정규식) 실행
+        System.out.println("두 번째 WordFilter 시작");
         for (Map.Entry<String, Integer> entry : report2.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue() + "개 발견");
         }
+        System.out.println("두 번째 WordFilter 종료");
     }
 }
--- src/WordFilter2.java
+++ src/WordFilter2.java
@@ -18,12 +18,10 @@ public class WordFilter2 {
     public Map<String, Integer> countBlackWords2(String userContent, String[] blackWordList) {
         String[] userContentSplit = userContent.split(" ");
         for (String blackWord : blackWordList) {
-            System.out.println(blackWord);
             int wordCount = 0;
             for (String splitContent : userContentSplit) {
                 Pattern pattern = Pattern.compile(blackWord, Pattern.CASE_INSENSITIVE); // 영어일 경우 Pattern.CASE_INSENSITIVE
                 Matcher matcher = pattern.matcher(splitContent);
-                System.out.println(matcher);
 
                 while (matcher.find()) {
                     wordCount++;
